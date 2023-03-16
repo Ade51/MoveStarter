71903c96bfe4cff295cf256a69bcfe72f158cada
Ade51 <iftimieadelin@gmail.com>
2023-03-16 16:02:43 +0200

    Initial commit

---
+++ MovieStarter/src/test/java/com/example/MovieStarter/MovieStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MovieStarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
